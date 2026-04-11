f39e80172cbc4c530d1491688ffa91c7466c797f
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-11 16:11:12 -0600

    add value objects to complete customer document

--- users/src/main/java/com/liverpool/users/domain/document/Customer.java
+++ users/src/main/java/com/liverpool/users/domain/document/Customer.java
@@ -1,22 +1,42 @@
 package com.liverpool.users.domain.document;
 
+import com.liverpool.users.domain.valueobject.Email;
+import com.liverpool.users.domain.valueobject.ShippingAddress;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
 
 @Document(collation = "customers")
 public class Customer {
 
     @Id
     private String id;
+
     @Field("first_name")
     @NotBlank
     private String firstName;
+
     @Field("last_name")
     @NotBlank
     private String lastName;
+
     @Field("second_last_name")
     @NotBlank
     private String secondLastName;
+
+    private Email email;
+
+    private ShippingAddress shippingAddress;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
