a80a93c4e966a26941c8ea9e1c058d93ce1b6eff
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-12 01:48:48 -0600

    delete unused imports

--- products/src/main/java/com/liverpool/products/domain/document/Product.java
+++ products/src/main/java/com/liverpool/products/domain/document/Product.java
@@ -12,7 +12,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 
