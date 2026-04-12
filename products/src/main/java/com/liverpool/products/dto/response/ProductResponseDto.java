76ee8161f31ba8307220ae9a5e3b41e2f246e89d
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-12 04:04:04 -0600

    modifications to execution completed, product service completed

--- products/src/main/java/com/liverpool/products/dto/response/ProductResponseDto.java
+++ products/src/main/java/com/liverpool/products/dto/response/ProductResponseDto.java
@@ -1,4 +1,6 @@
 package com.liverpool.products.dto.response;
 
-public record ProductResponseDto (String id, String sku, String name, String description, String category, Boolean price, Integer stock) {
+import java.math.BigDecimal;
+
+public record ProductResponseDto (String id, String sku, String name, String description, String category, BigDecimal price, Integer stock) {
 }
