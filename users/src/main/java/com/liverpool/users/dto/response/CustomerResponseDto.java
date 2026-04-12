b043faf5c07877a7186f05ef4286b920f9fa75e4
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-12 02:49:03 -0600

    delete unused import

--- users/src/main/java/com/liverpool/users/dto/response/CustomerResponseDto.java
+++ users/src/main/java/com/liverpool/users/dto/response/CustomerResponseDto.java
@@ -1,7 +1,5 @@
 package com.liverpool.users.dto.response;
 
-import com.liverpool.users.domain.valueobject.ShippingAddress;
-
 public record CustomerResponseDto(String id, String firstName, String lastName, String secondLastName, String email, ShippingAddressDto shippingAddress) {
     public record ShippingAddressDto(
             String street,
