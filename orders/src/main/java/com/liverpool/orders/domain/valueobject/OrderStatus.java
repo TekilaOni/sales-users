b913a308629ea92f813f9af93e14f70947d220c2
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-12 12:23:09 -0600

    creation of enum status and reuse of price class

---
+++ orders/src/main/java/com/liverpool/orders/domain/valueobject/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.liverpool.orders.domain.valueobject;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ orders/src/main/java/com/liverpool/orders/domain/valueobject/Price.java
@@ -0,0 +1,38 @@
+package com.liverpool.orders.domain.valueobject;
+
+import com.liverpool.products.domain.util.DomainValidator;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.data.annotation.PersistenceCreator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Getter
+@EqualsAndHashCode
+public class Price {
+
+    private final BigDecimal amount;
+
+    @PersistenceCreator
+    private Price(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    private Price(){
+        this.amount = null;
+    }
+
+    public static Price of(BigDecimal amount) {
+        DomainValidator.validateNotNull(amount,"El precio es obligatorio");
+        DomainValidator.validateNotNegative(amount,BigDecimal.ZERO,"El precio no puede ser negativo");
+        return new Price(amount);
+    }
+
+    public Price multiply(Integer quantity) {
+        return new Price(
+                this.amount.multiply(BigDecimal.valueOf(quantity))
+                        .setScale(2, RoundingMode.HALF_UP)
+        );
+    }
+}
