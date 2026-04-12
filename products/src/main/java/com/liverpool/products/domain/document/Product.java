c426670ace8d57a9c196f64d6544ca390d087ee7
Ramiro <ramiro.diaz@beexponentialgroup.com>
2026-04-12 01:45:54 -0600

    created utilery class and Product document

---
+++ products/src/main/java/com/liverpool/products/domain/document/Product.java
@@ -0,0 +1,47 @@
+package com.liverpool.products.domain.document;
+
+import com.liverpool.products.domain.valueobject.Price;
+import com.liverpool.products.domain.valueobject.Sku;
+import com.liverpool.products.domain.valueobject.Stock;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "products")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private Sku sku;
+
+    private String name;
+
+    private String description;
+
+    private String category;
+
+    private Price price;
+
+    private Stock stock;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
